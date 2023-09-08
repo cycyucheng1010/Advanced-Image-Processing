2e1a4a7757b8c21d86837b80abeb2d1d406dbb6e
Rick Chen <rick20001010@gmail.com>
2023-09-08 19:03:24 +0800

    pyqt5 init

---
+++ app.py
@@ -0,0 +1,19 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
+
+def window():
+	app = QApplication(sys.argv)
+	widget = QWidget()
+	textLabel = QLabel(widget)
+	textLabel.setText("Hello World!")
+	textLabel.move(110,85)
+	QIcon('NTNU.icon')
+	widget.setGeometry(50,50,320,200)
+	widget.setWindowTitle("AIS61247014S")
+	widget.show()
+	sys.exit(app.exec_())
+	
+if __name__ == '__main__':
+	window()
